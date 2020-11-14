f84fd70ada28c14c7b6f96987332bd3e1b42d397
FarheenIqbal <73950223+FarheenIqbal@users.noreply.github.com>
2020-11-14 21:30:26 +0530

    Add files via upload

--- maximumOfThree.js
+++ maximumOfThree.js
@@ -1,13 +1,11 @@
-const maxOfThree = function (x, y, z) {
-  if (x > y && x > z) {
-    return x
-  } else if (y > x && y > z) {
-    return y
-  } else {
-    return z
-  }
+const max3 = function (x, y, z) {
+  return max2(x, max2(y, z))
 }
 
-console.log(maxOfThree(5, 7, 3))
-console.log(maxOfThree(15, 7, 3))
-console.log(maxOfThree(5, 7, 13))
+const max2 = function (x, y) {
+  return x > y ? x : y
+}
+
+console.log(max3(5, 7, 3))
+console.log(max3(15, 7, 3))
+console.log(max3(5, 7, 13))
