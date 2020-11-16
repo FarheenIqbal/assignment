424b6e50067dc2bd35a697866ee9a356bf3283d0
FarheenIqbal <73950223+FarheenIqbal@users.noreply.github.com>
2020-11-16 18:08:25 +0530

    Add files via upload

---
+++ DoublyLinkedList.js
@@ -0,0 +1,14 @@
+
+
+let list = null;
+
+function insert(list, value, pos) {
+    let node = { 
+        prev : null,
+        data : value,
+        next : null
+    };
+    if(pos <= 0 && pos > length(list)) return;
+
+    
+}
